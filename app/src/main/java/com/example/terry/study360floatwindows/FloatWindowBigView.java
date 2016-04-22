e93e76b278e6032fc1a8bc8a8d2f960dabf0a5a1
Terry <bthvi@outlook.com>
2016-04-22 20:17:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/terry/study360floatwindows/FloatWindowBigView.java
@@ -0,0 +1,52 @@
+package com.example.terry.study360floatwindows;
+
+/**
+ * Created by Terry on 2016/4/17.
+ */
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class FloatWindowBigView extends LinearLayout {
+
+    /**
+     * 记录大悬浮窗的宽度
+     */
+    public static int viewWidth;
+
+    /**
+     * 记录大悬浮窗的高度
+     */
+    public static int viewHeight;
+
+    public FloatWindowBigView(final Context context) {
+        super(context);
+        LayoutInflater.from(context).inflate(R.layout.float_windows, this);
+        View view = findViewById(R.id.big_windows);
+        viewWidth = view.getLayoutParams().width;
+        viewHeight = view.getLayoutParams().height;
+        Button close = (Button) findViewById(R.id.close);
+        Button back = (Button) findViewById(R.id.back);
+        close.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 点击关闭悬浮窗的时候，移除所有悬浮窗，并停止Service
+                MyWindowManager.removeBigWindow(context);
+                MyWindowManager.removeSmallWindow(context);
+                Intent intent = new Intent(getContext(), FloatWindowsSeverice.class);
+                context.stopService(intent);
+            }
+        });
+        back.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 点击返回的时候，移除大悬浮窗，创建小悬浮窗
+                MyWindowManager.removeBigWindow(context);
+                MyWindowManager.createSmallWindow(context);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/terry/study360floatwindows/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.terry.study360floatwindows;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Button start = (Button) findViewById(R.id.button);
+         start.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+
+                 Intent i = new Intent(MainActivity.this,FloatWindowsSeverice.class);
+                 startService(i);
+                 finish();
+             }
+         });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
